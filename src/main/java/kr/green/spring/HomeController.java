b84e9e8ea6f236b37c13ec8647efb0a713896c27
jrntky <jnrkty1@naver.com>
2019-01-16 11:45:50 +0900

    주석추가

--- src/main/java/kr/green/spring/HomeController.java
+++ src/main/java/kr/green/spring/HomeController.java
@@ -24,19 +24,22 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		/* 컨트롤러에서 jsp로 데이터를 전달하기 위해서
+		 * 1. 매개변수에 Model 객체를 추가 : Model model
+		 * 2. model.addAttribute()를 통해 데이터 전달
+		 * 	  model.addAttribute("jsp에서 받는 이름", "전달할데이터");
+		 *  */
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", "서버시간");
 		
 		return "home";
 	}
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public String homePost(String name){
+		/* jsp에서 넘겨준 데이터를 받기 위해서는 태그의 속성 name과 일치하는 이름을 매개변수의 이름으로 설정한다
+		 * 
+		 * 
+		 *  */
 		System.out.println("jsp에서 넘어온 값 : " +name);
 		return "redirect:/";
 	}
